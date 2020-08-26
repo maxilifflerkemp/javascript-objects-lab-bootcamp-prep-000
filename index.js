fbf993a5080afae7dd02d006fa64399f6e908903
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-26 20:38:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function updateObjectWithKeyAndValue(object, key, value) { return Object.assign(
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 object[key] = value;
 return object }
+
+function deleteFromObjectByKey(object, key) {
+var newObj = Object.assign({}, obj);
+delete newObj[key];
+return newObj;
+ }
