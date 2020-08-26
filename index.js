7acf5e032d07fa7fecf1c9b0dd726b85e9c2bfcf
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-26 20:28:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 var recipes = newObject();
 
-function updateObjectWithKeyAndValue(object, key, value) { return Object.assign({}, object, {[key]}: value} )}
+function updateObjectWithKeyAndValue(object, key, value) { return Object.assign({}, object, {[key]: value} )}
