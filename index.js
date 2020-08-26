0a1cb586b711f3a5ccaedd2b360c6690e6f93559
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-26 20:39:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,3 +11,8 @@ var newObj = Object.assign({}, object);
 delete newObj[key];
 return newObj;
  }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+delete object[key];
+return object
+}
