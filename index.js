969be6ad441352ad7e38a887e01940d732419503
Maximiliane Liffler <maxi-liffler@web.de>
2020-08-26 20:32:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var recipes = newObject();
 function updateObjectWithKeyAndValue(object, key, value) { return Object.assign({}, object, {[key]: value} )}
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-object.key = value;
+object[key] = value;
 return object }
